eb22d8b7a302149f365fd4801ff5c942453828aa
0xdeenz <0xdeenz@protonmail.com>
2023-02-08 17:33:37 +0100

    use same packProof as in Semaphore

--- packages/proof/src/helpers/packProof.ts
+++ packages/proof/src/helpers/packProof.ts
@@ -1,9 +1,19 @@
 import { Proof, SnarkJSProof } from "../types";
 
-export function packProof(originalProof: SnarkJSProof): Proof {
-    return {
-        a: [originalProof.pi_a[0], originalProof.pi_a[1]],
-        b: [[originalProof.pi_b[0][1], originalProof.pi_b[0][0]], [originalProof.pi_b[1][1], originalProof.pi_b[1][0]]],
-        c: [originalProof.pi_c[0], originalProof.pi_c[1]]
-    }
+/**
+ * Packs a proof into a format compatible with Block Qualified.
+ * @param originalProof The proof generated with SnarkJS.
+ * @returns The proof compatible with Block Qualified and Semaphore.
+ */
+export default function packProof(originalProof: SnarkJSProof): Proof {
+    return [
+        originalProof.pi_a[0],
+        originalProof.pi_a[1],
+        originalProof.pi_b[0][1],
+        originalProof.pi_b[0][0],
+        originalProof.pi_b[1][1],
+        originalProof.pi_b[1][0],
+        originalProof.pi_c[0],
+        originalProof.pi_c[1]
+    ]
 }
