227ac535388c31f58dfde2b18cf8a53c42039dcf
0xdeenz <0xdeenz@protonmail.com>
2023-02-08 17:34:19 +0100

    add helper functions

---
+++ packages/proof/src/helpers/getGradeCommitment.ts
@@ -0,0 +1,65 @@
+import { Group, Member } from "@semaphore-protocol/group"
+import type { Identity } from "@semaphore-protocol/identity"
+import { TEST_HEIGHT } from "../constants";
+import { Poseidon, buildPoseidon } from "./buildPoseidon";
+import { GradeCommitment, TestVariables } from "../types";
+
+/**
+ * Finds the grade a user obtained from their identity and grade group by doing a brute force search through all the possible grades they could obtain.
+ * @param gradeGroup The Semaphore group for the grade group.
+ * @param identity The Semaphore identity that will be associated with the solution.
+ * @param multipleChoiceWeight: Contribution of the multiple choice component towards the final grade.
+ * @param nQuestions: Number of questions that make up the open answer component.
+ * @returns The grade the user obtained.
+ */
+export default async function getGradeCommitment(
+    { trapdoor, nullifier }: Identity,
+    gradeGroup: Group,
+    multipleChoiceWeight: number,
+    nQuestions: number,
+): Promise<GradeCommitment> {
+    let poseidon: Poseidon = await buildPoseidon();
+
+    let identitySecret = poseidon([nullifier, trapdoor])
+
+    let gradeCommitmentValue: bigint;
+    let gradeCommitmentIndex: number;
+    let weightedGrade: number;
+    let grade: number;
+
+    // There are 2 ** (TEST_HEIGHT + 1) + 2 possible grades the user might have obtained, we simply have to iterate through these and find if the grade commitment is in the tree
+    var i = 0, n = 2 ** TEST_HEIGHT;
+    while (i <= n) {
+        grade = Math.floor((100 - multipleChoiceWeight) * i / nQuestions)
+        weightedGrade = grade * nQuestions
+        gradeCommitmentValue = poseidon([identitySecret, weightedGrade])
+        gradeCommitmentIndex = gradeGroup.indexOf(gradeCommitmentValue)
+        
+        if (gradeCommitmentIndex !== -1) {
+            return {
+                gradeCommitmentValue,
+                gradeCommitmentIndex,
+                weightedGrade,
+                grade
+            }
+        }
+
+        grade += multipleChoiceWeight
+        weightedGrade = grade * nQuestions
+        gradeCommitmentValue = poseidon([identitySecret, weightedGrade])
+        gradeCommitmentIndex = gradeGroup.indexOf(gradeCommitmentValue)
+
+        if (gradeCommitmentIndex !== -1) {
+            return {
+                gradeCommitmentValue,
+                gradeCommitmentIndex,
+                weightedGrade,
+                grade
+            }
+        }
+
+        i++;
+    }
+
+    throw new Error("The user did not obtain a grade for this test")
+}
---
+++ packages/proof/test/helpers.test.ts
@@ -0,0 +1,144 @@
+import { buildPoseidon, generateCredentialOwnershipProof, generateOpenAnswers, getGradeCommitment, hash, N_LEVELS, Poseidon, TEST_HEIGHT } from "@bq-core/proof"
+import { formatBytes32String } from "@ethersproject/strings"
+import { Group } from "@semaphore-protocol/group"
+import { Identity } from "@semaphore-protocol/identity"
+import { FullProof } from "@semaphore-protocol/proof"
+import * as chai from 'chai'    
+import chaiAsPromised from 'chai-as-promised'
+import { utils } from "ethers";
+import { getCurveFromName } from "ffjavascript"
+import packProof from "../src/helpers/packProof"
+import unpackProof from "../src/helpers/unpackProof"
+
+chai.use(chaiAsPromised)
+
+describe("Helper functions", () => {
+    let poseidon: Poseidon 
+
+    const externalNullifier = formatBytes32String("Topic")
+    const signal = formatBytes32String("Hello world")
+
+    const snarkArtifacts = {
+        wasmFilePath: './snark-artifacts/semaphore.wasm',
+        zkeyFilePath: `./snark-artifacts/semaphore.zkey`
+    }
+
+    const identity = new Identity()
+
+    let fullProof: FullProof
+    let curve: any
+
+    const expect = chai.expect
+
+    before(async () => {
+        poseidon = await buildPoseidon();
+
+        curve = await getCurveFromName("bn128")
+
+        const group = new Group(0, N_LEVELS)
+        group.addMembers([BigInt(1), BigInt(2), identity.commitment])
+
+        fullProof = await generateCredentialOwnershipProof(identity, group, externalNullifier, signal, snarkArtifacts) as FullProof
+    })
+
+    after(async () => {
+        await curve.terminate()
+    })
+
+    describe("hash", () => {
+        it("Should hash the signal value correctly", async () => {
+            const signalHash = hash(signal)
+
+            expect(signalHash.toString()).to.be.equal(
+                "8665846418922331996225934941481656421248110469944536651334918563951783029"
+            )
+        })
+
+        it("Should hash the external nullifier value correctly", async () => {
+            const externalNullifierHash = hash(externalNullifier)
+
+            expect(externalNullifierHash.toString()).to.be.equal(
+                "244178201824278269437519042830883072613014992408751798420801126401127326826"
+            )
+        })
+
+        it("Should hash a number", async () => {
+            expect(hash(2).toString()).to.be.equal(
+                "113682330006535319932160121224458771213356533826860247409332700812532759386"
+            )
+        })
+
+        it("Should hash a big number", async () => {
+            expect(hash(BigInt(2)).toString()).to.be.equal(
+                "113682330006535319932160121224458771213356533826860247409332700812532759386"
+            )
+        })
+
+        it("Should hash an hex number", async () => {
+            expect(hash("0x2").toString()).to.be.equal(
+                "113682330006535319932160121224458771213356533826860247409332700812532759386"
+            )
+        })
+
+        it("Should hash an string number", async () => {
+            expect(hash("2").toString()).to.be.equal(
+                "113682330006535319932160121224458771213356533826860247409332700812532759386"
+            )
+        })
+
+        it("Should hash an array", async () => {
+            expect(hash([2]).toString()).to.be.equal(
+                "113682330006535319932160121224458771213356533826860247409332700812532759386"
+            )
+        })
+    })
+
+    describe("packProof and unpackProof", () => {
+        it("Should return a packed proof", async () => {
+            fullProof.proof
+            const originalProof = unpackProof(fullProof.proof)
+            const proof = packProof(originalProof)
+
+            expect(proof).to.deep.equal(fullProof.proof)
+        })
+    })
+
+    describe("generateOpenAnswers", () => {
+        it("Throws when giving it more answers than supported", () => {
+            expect(
+                () => generateOpenAnswers(Array(2 ** TEST_HEIGHT + 1).fill('deenz'))
+            ).to.throw("More answers were given than supported")
+        })
+
+        it("Fills up an incomplete open answers array", () => {
+            const fullOpenAnswers = generateOpenAnswers(['deenz'])
+            const expectedOpenAnswers = Array(2 ** TEST_HEIGHT).fill(BigInt(utils.keccak256(utils.toUtf8Bytes(""))))
+            expectedOpenAnswers[0] = BigInt(utils.keccak256(utils.toUtf8Bytes("deenz")))
+
+            expect(fullOpenAnswers).to.deep.equal(expectedOpenAnswers)
+        })
+    })
+
+    describe("getGradeCommitment", () => {
+        it("Gets the correct grade commitment from a tree", async () => {
+            const gradeGroup = new Group(0, N_LEVELS)
+
+            const multipleChoiceWeight = 40;
+            const nQuestions = 10;
+    
+            const weightedGrade = multipleChoiceWeight * nQuestions + (100 - multipleChoiceWeight) * (60 + nQuestions - 64)
+            const _gradeCommitment = poseidon([poseidon([identity.nullifier, identity.trapdoor]), weightedGrade])
+    
+            gradeGroup.addMembers([BigInt(1), BigInt(2), _gradeCommitment])
+    
+            const gradeCommitment = await getGradeCommitment(identity, gradeGroup, multipleChoiceWeight, nQuestions)
+    
+            expect(gradeCommitment).to.deep.equal({
+                gradeCommitmentValue: _gradeCommitment,
+                gradeCommitmentIndex: gradeGroup.indexOf(_gradeCommitment),
+                weightedGrade,
+                grade: Math.floor(weightedGrade / nQuestions)
+            })
+        })
+    })
+})
