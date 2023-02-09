da149fa1d7bb3b34b4c32fc0806c2b3e2eba5228
0xdeenz <0xdeenz@protonmail.com>
2023-02-09 18:40:51 +0100

    include all exports

--- packages/proof/src/index.ts
+++ packages/proof/src/index.ts
@@ -1,9 +1,29 @@
 
 import { Poseidon, buildPoseidon } from "./helpers/buildPoseidon"
+import hash from "./helpers/hash"
 import rootFromLeafArray from "./helpers/tree"
 import generateOpenAnswers from "./helpers/generateOpenAnswers"
-import generateTestProof from "./generateTestProof"
+import getGradeCommitment from "./helpers/getGradeCommitment"
+import generateTestProof from "./provers/generateTestProof"
+import generateCredentialOwnershipProof from "./provers/generateCredentialOwnershipProof"
+import generateGradeClaimProof from "./provers/generateGradeClaimProof"
+import verifyCredentialOwnershipProof from "./verifiers/verifyCredentialOwnershipProof"
+import verifyGradeClaimProof from "./verifiers/verifyGradeClaimProof"
+import verifyTestProof from "./verifiers/verifyTestProof"
 
-export { Poseidon, buildPoseidon, rootFromLeafArray, generateTestProof, generateOpenAnswers }
 export * from "./constants"
 export * from "./types"
+export { 
+    Poseidon, 
+    buildPoseidon, 
+    hash, 
+    rootFromLeafArray, 
+    generateOpenAnswers,
+    getGradeCommitment,
+    generateTestProof, 
+    generateCredentialOwnershipProof,
+    generateGradeClaimProof,
+    verifyCredentialOwnershipProof,
+    verifyGradeClaimProof,
+    verifyTestProof
+}
