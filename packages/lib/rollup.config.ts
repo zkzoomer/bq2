ed3fa7d2f1249e4e22da5db31f7b610eefa60d3b
0xdeenz <0xdeenz@protonmail.com>
2023-04-19 18:03:07 +0200

    fix build

--- packages/lib/rollup.config.ts
+++ packages/lib/rollup.config.ts
@@ -1,5 +1,3 @@
-import commonjs from "@rollup/plugin-commonjs"
-import { nodeResolve }  from "@rollup/plugin-node-resolve"
 import typescript from "rollup-plugin-typescript2"
 import json from "@rollup/plugin-json"
 import * as fs from "fs"
@@ -22,8 +20,6 @@ export default {
             tsconfig: "./build.tsconfig.json",
             useTsconfigDeclarationDir: true
         }),
-        json(),
-        commonjs(),
-        nodeResolve({ preferBuiltins: true, mainFields: ['browser'] }),
+        json()
     ]
 };
